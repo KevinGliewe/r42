fe63dcaaaa693ba8a4424c4ddd45063b0ecabeac
Kevin Gliewe <kevingliewe@gmail.com>
2024-01-23 21:35:17 +0000

    Add C++

--- src/langs.rs
+++ src/langs.rs
@@ -8,11 +8,12 @@ pub struct Language {
     pub expression_writer: r42t::Writer,
 }
 
-pub const LENGUAGES: [Language; 4] = [
+pub const LENGUAGES: [Language; 5] = [
     Language {name: "Rust", extension: "rs", template_writer: rust_template_writer, expression_writer: rust_expression_writer},
     Language {name: "C#", extension: "cs", template_writer: csharp_template_writer, expression_writer: csharp_expression_writer},
     Language {name: "Java", extension: "java", template_writer: java_template_writer, expression_writer: java_expression_writer},
     Language {name: "JavaScript", extension: "js", template_writer: javascript_template_writer, expression_writer: javascript_expression_writer},
+    Language {name: "C++", extension: "cpp", template_writer: cpp_template_writer, expression_writer: cpp_expression_writer},
 ];
 
 // Rust
@@ -59,6 +60,18 @@ fn javascript_template_writer(code_buffer: &mut String, template_buffer: &String
 }
 
 fn javascript_expression_writer(code_buffer: &mut String, expression_buffer: &String){
+    if expression_buffer.len() == 0 { return;}
+    code_buffer.push_str(format!("\nwrite({});\n", expression_buffer).as_str());
+}
+
+// C++
+
+fn cpp_template_writer(code_buffer: &mut String, template_buffer: &String){
+    if template_buffer.len() == 0 { return; }
+    code_buffer.push_str(format!("\nwrite(\"{}\");\n", template_buffer).as_str());
+}
+
+fn cpp_expression_writer(code_buffer: &mut String, expression_buffer: &String){
     if expression_buffer.len() == 0 { return;}
     code_buffer.push_str(format!("\nwrite({});\n", expression_buffer).as_str());
 }
